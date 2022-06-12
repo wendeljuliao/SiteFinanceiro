893bca4d418c78c75af33c1b0bc4d99995b2e26a
Wendel <wendelljuliao@hotmail.com>
2022-06-11 22:58:27 -0300

    graficos feitos

---
+++ priv/static/assets/js/linechart.js
@@ -0,0 +1,37 @@
+//loading charjs
+const Chart = require("chart.js");
+
+//A Canvas dom element with ID "lineChart" is where our chart will display
+var ctx = document.getElementById("lineChart").getContext("2d");
+
+var myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    //we make sure of the following variable to available in the template that uses this JS file and it act as X-Axis
+    labels: [1, 2, 3],
+    datasets: [
+      {
+        label: "Receitas & Meses ",
+
+        // Adjust the colors and Background here if you need
+        backgroundColor: "rgba(155, 89, 182,0.2)",
+        borderColor: "rgba(142, 68, 173,1.0)",
+        pointBackgroundColor: "rgba(142, 68, 173,1.0)",
+
+        //we make sure of the following variable to available in the template that uses this JS
+        data: [1, 2, 3],
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  },
+});
